9691444534ccac5a996b4d480dd6287fe02d9a85
Mattia Ripamonti <mattia.ripamonti@bitrock.it>
2019-07-03 21:27:30 +0200

    yarn upgrade

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
 
-exports.updateDoc = functions.firestore
+// firebase deploy --only functions
+
+exports.updateDocCount = functions.firestore
   .document('/stores/{userId}/location/{locationId}/items/{itemId}')
-  .onUpdate((change, context) => {
-    console.log(change, context);
+  .onWrite((change, context) => {
+
+    const userId = context.params.userId;
+    const locationId = context.params.locationId;
+
+    const docRef = firebase.firestore()
+      .collection('stores').doc(userId)
+      .collection('location').doc(locationId);
+
+    const loc = docRef.get();
+
+    return docRef.collection('items').get().then(store => {
+      let count = 0;
+      const items = store.docs;
+
+      items.forEach((item) => {
+        const evaluatedItem = item.data();
+        if (evaluatedItem.amount > 0) {
+          count++;
+        }
+      });
+
+      const data = { ...loc.data(), count };
+      return docRef.update(data);
+    });
+
   });

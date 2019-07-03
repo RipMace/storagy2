9fe442842bc3878c6bc9048b2f290b47488fcd16
Mattia Ripamonti <mattia.ripamonti@bitrock.it>
2019-07-03 16:23:03 +0200

    fix functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 
-exports.myFunctionName = functions.firestore
-  .document('/stores/{userId}/location/{locationId}/items/{itemId}').onUpdate((change, context) => {
+exports.updateDoc = functions.firestore
+  .document('/stores/{userId}/location/{locationId}/items/{itemId}')
+  .onUpdate((change, context) => {
     console.log(change, context);
   });
